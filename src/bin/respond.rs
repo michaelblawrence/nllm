d736f5de379969faa57b0fb201911ff956fdad4b
Michael Lawrence <mblawrence27@gmail.com>
2023-04-15 14:08:02 +0900

    add command: reload latest model for inferrence

--- src/bin/respond.rs
+++ src/bin/respond.rs
@@ -10,9 +10,16 @@ use plane::ml::{embeddings::builder::EmbeddingBuilder, JsRng};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let json = match read_model_file(args.first()) {
+    let fpath_arg = args.first();
+    let mut fpath_arg = fpath_arg.cloned();
+
+    while run(&mut fpath_arg) {}
+}
+
+fn run(model_fpath: &mut Option<String>) -> bool {
+    let json = match read_model_file(model_fpath.as_ref()) {
         Some(value) => value,
-        None => return,
+        None => return false,
     };
 
     let rng = Rc::new(JsRng::default());
@@ -20,6 +27,12 @@ fn main() {
         .expect("failed to rebuild state from snapshot")
         .build()
         .expect("failed to rebuild instance from snapshot state");
+
+    println!("..");
+    println!(
+        "Loaded model from '{}'",
+        model_fpath.clone().unwrap_or_default()
+    );
 
     let char_mode = embedding
         .vocab()
@@ -29,19 +42,30 @@ fn main() {
     let append_space = char_mode && embedding.vocab().contains_key(" ");
 
     println!("Starting... (character input mode = {char_mode})");
+    println!("   - Commands available: [ '.load <path>' | '.reload' | '.quit' ]");
+    println!("");
 
     let stdin = io::stdin();
 
     loop {
-        println!("..");
-        println!("..");
-        print!("Enter prompt for model (or type 'quit'): ");
+        println!("");
+        print!("Enter prompt for model: ");
         io::stdout().flush().unwrap();
+        
         let input_txt = read_line(&stdin);
-
-        if &input_txt == "quit" {
-            break;
-        }
+        let input_txt = match process_repl_commands(&input_txt, model_fpath.as_ref()) {
+            CliReplActions::ProcessInput => input_txt,
+            CliReplActions::Reload(input_path) => {
+                model_fpath.replace(input_path);
+                return true;
+            }
+            CliReplActions::Quit => {
+                return false;
+            }
+            CliReplActions::Reprompt => {
+                continue;
+            }
+        };
 
         let context_tokens: Vec<String> =
             to_context_tokens(&input_txt, char_mode, append_space).collect();
@@ -66,8 +90,7 @@ fn main() {
             response.join(" ")
         };
 
-        println!("..");
-        println!("Model Response: {response}");
+        println!("Model Response:        {response}");
     }
 }
 
@@ -119,4 +142,67 @@ fn to_context_tokens<'a>(
             .collect()
     };
     context_tokens.into_iter()
+}
+
+
+
+enum CliReplActions {
+    ProcessInput,
+    Reload(String),
+    Quit,
+    Reprompt,
+}
+
+fn process_repl_commands(input_txt: &str, model_fpath: Option<&String>) -> CliReplActions {
+    if input_txt == ".quit" {
+        return CliReplActions::Quit;
+    }
+
+    if input_txt.starts_with(".load") {
+        let input_tokens = input_txt.split_ascii_whitespace().collect::<Vec<_>>();
+        if let [".load", input_path] = input_tokens[..] {
+            let input_path = input_path.to_string();
+            return CliReplActions::Reload(input_path);
+        } else {
+            println!("Command usage: .load <MODEL_FILE_PATH>");
+            return CliReplActions::Reprompt;
+        }
+    }
+
+    if input_txt == ".reload" {
+        if let Some(curr_fpath) = model_fpath {
+            let curr_fpath = Path::new(&curr_fpath);
+            let round_models: Vec<_> = curr_fpath
+                .parent()
+                .expect("failed to traverse path to model dir")
+                .read_dir()
+                .expect("failed to open model dir")
+                .flatten()
+                .filter_map(|f| {
+                    Some((
+                        f.file_name()
+                            .to_string_lossy()
+                            .split_once("model-r")
+                            .and_then(|split| split.1.split('-').next())
+                            .and_then(|round_str| round_str.parse::<usize>().ok())?,
+                        f.path(),
+                    ))
+                })
+                .collect();
+
+            let latest_round_model = round_models.iter().max_by_key(|(round, _)| round);
+            if let Some((_, latest_model_fpath)) = latest_round_model {
+                let latest_model_fpath = latest_model_fpath.as_path();
+                if latest_model_fpath != curr_fpath {
+                    let input_path = latest_model_fpath.to_string_lossy().to_string();
+                    return CliReplActions::Reload(input_path);
+                }
+            }
+        }
+
+        println!("No latest model found in model directory");
+        return CliReplActions::Reprompt;
+    }
+
+    CliReplActions::ProcessInput
 }
