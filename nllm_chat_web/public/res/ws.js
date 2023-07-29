11c8afd351c93dcd8a88f548f23692857b871808
Michael Lawrence <mblawrence27@gmail.com>
2023-07-30 01:31:11 +0900

    extract js

---
+++ nllm_chat_web/public/res/ws.js
@@ -0,0 +1,35 @@
+initKeepAliveSocket = () => {
+    const keepalive = new WebSocket("ws://localhost:3000/keepalive");
+    keepalive.onopen = function () {
+        console.log("keepalive connection established.. will restart when code changed");
+    };
+    keepalive.onclose = function () {
+        let restartConnection = {
+            restart: () => { },
+            handle: null
+        }
+
+        restartConnection.restart = () => {
+            if (restartConnection.handle) {
+                clearTimeout(restartConnection.handle);
+                restartConnection.handle = null;
+            }
+            console.log("connection to server lost, attempting to reestablish connection...");
+            restartConnection.handle = setTimeout(() => {
+                const keepalive = new WebSocket("ws://localhost:3000/keepalive");
+                keepalive.onopen = function () {
+                    console.log("server restarted, reloading page...");
+                    window.location.reload();
+                };
+                keepalive.onerror = function () {
+                    restartConnection.restart();
+                }
+                keepalive.onclose = function () {
+                    restartConnection.restart();
+                }
+            }, 3000);
+        };
+
+        restartConnection.restart();
+    }
+};
