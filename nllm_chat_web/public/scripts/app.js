fb0e23a853624e7b7a4ea829de86fd5a8a6a8d25
Michael Lawrence <mblawrence27@gmail.com>
2023-07-30 17:50:05 +0900

    extract components

--- nllm_chat_web/public/scripts/app.js
+++ nllm_chat_web/public/scripts/app.js
@@ -7,6 +7,14 @@ import { render } from "https://cdn.skypack.dev/solid-js/web";
 import html from "https://cdn.skypack.dev/solid-js/html";
 
 function App() {
+    return html`
+        <${TopBar} />
+        <${Conversation} />
+        <${Footer} />
+    `;
+}
+
+function Conversation() {
     const [messageInputValue, setMessageInputValue] = createSignal("");
     const [usernameValue, setUsernameValue] = createSignal("user1");
     const [textAreaValue, setTextAreaValue] = createSignal("");
@@ -14,23 +22,36 @@ function App() {
     const [wsInvokeSend, setWsInvokeSend] = createSignal((_msg) => { });
 
     const chatTextArea = html`
-    <textarea class="block p-2.5 w-full h-[250px] text-sm text-gray-900 bg-gray-50 rounded-lg leading-tight border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        rows="4" readonly>
-            ${textAreaValue}
-    </textarea>
+        <textarea class="block p-2.5 w-full h-[250px] text-sm text-gray-900 bg-gray-50 rounded-lg leading-tight border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            rows="4" readonly>
+                ${textAreaValue}
+        </textarea>
     `;
 
     if (initKeepAliveSocket) {
         initKeepAliveSocket();
     }
 
+    window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+        setTextAreaValue(value => value + "Error occured: " + errorMsg + "\n\n");
+        return false;
+    }
+
     const onConnect = () => {
+        if (connectDisabled()) {
+            return;
+        }
+
         setConnectDisabled(true);
         const websocket = new WebSocket("ws://localhost:3000/websocket");
 
         websocket.onopen = function () {
             console.log("connection opened");
             websocket.send(usernameValue());
+        }
+
+        websocket.onerror = function (e) {
+            setTextAreaValue(value => value + "Connection error occured\n\n");
         }
 
         websocket.onclose = function () {
@@ -88,9 +109,7 @@ function App() {
     const onUsernameKeyDown = e => {
         if (e.key == "Enter") {
             e.preventDefault();
-            if (!connectDisabled()) {
-                onConnect();
-            }
+            onConnect();
         }
     };
 
@@ -111,33 +130,6 @@ function App() {
     };
 
     return html`
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="#" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ChatNLLM</span>
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                    </svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Chat</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/michaelblawrence/nllm/blob/master/README.md" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/michaelblawrence/nllm" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Code</a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div class="flex flex-col max-w-screen-xl mx-auto p-4 space-y-2">
             <h2 class="text-2xl font-extrabold dark:text-white">Chat with others and with AI!</h2>
             
@@ -149,13 +141,15 @@ function App() {
                         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
                     </svg>
                     </div>
-                    <input class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required placeholder="Username" type="text" id="website-admin"
-                        value=${usernameValue}
-                        onInput=${e => setUsernameValue(e.currentTarget.value)}
-                        onKeyDown=${onUsernameKeyDown}>
+                    <form onsubmit=${onConnect}>
+                        <input class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required placeholder="Username" type="text" id="website-admin"
+                            value=${usernameValue}
+                            onInput=${e => setUsernameValue(e.currentTarget.value)}
+                            onKeyDown=${onUsernameKeyDown}>
+                    </form>
                     <button class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        type="button" disabled=${connectDisabled} onClick=${onConnect}>
+                        type="submit" disabled=${connectDisabled} onClick=${onConnect}>
                             Join Chat
                     </button>
                 </div>
@@ -165,7 +159,7 @@ function App() {
 
             <div>
                 <label for="chat" class="sr-only">Your message</label>
-                <div class="flex items-center px-3 py-2 rounded-lg border-gray-300 bg-gray-50 dark:bg-gray-700">
+                <div class="flex items-center px-3 py-2 rounded-lg border border-gray-300 bg-gray-50 dark:bg-gray-700">
                     <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
                             <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
@@ -196,7 +190,43 @@ function App() {
                 </div>
             </div>
         </div>
+    `;
+}
 
+function TopBar() {
+    return html`
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ChatNLLM</span>
+                </a>
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Chat</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/michaelblawrence/nllm/blob/master/README.md" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/michaelblawrence/nllm" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Code</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `;
+}
+
+function Footer() {
+    return html`
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
