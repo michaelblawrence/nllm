af5bd5df4f2ced5148a80469aba438c804d880e4
Michael Lawrence <mblawrence27@gmail.com>
2023-08-01 14:14:12 +0900

    auto-connect on send msg + anon usernames

--- nllm_chat_web/public/scripts/app.js
+++ nllm_chat_web/public/scripts/app.js
@@ -16,8 +16,10 @@ function App() {
 }
 
 function Conversation() {
+    const initUserIndex = `1${Math.floor(Math.random() * 999)}`;
+    const initUserId = `${"00000".substring(initUserIndex.length)}${initUserIndex}`;
     const [messageInputValue, setMessageInputValue] = createSignal("");
-    const [usernameValue, setUsernameValue] = createSignal("user1");
+    const [usernameValue, setUsernameValue] = createSignal("anon_1" + initUserId);
     const [textAreaValue, setTextAreaValue] = createSignal("");
     const [connectDisabled, setConnectDisabled] = createSignal(false);
     const [wsInvokeSend, setWsInvokeSend] = createSignal((_msg) => { });
@@ -34,79 +36,87 @@ function Conversation() {
         return false;
     }
 
-    const onConnect = () => {
+    const wsStartConnection = async () => {
         if (connectDisabled()) {
-            return;
+            return Promise.resolve(false);
         }
 
         setConnectDisabled(true);
-        const websocket = new WebSocket(document.location.origin.replace(/^http/, "ws") + "/websocket");
+        return new Promise((resolve, reject) => {
+            const websocket = new WebSocket(document.location.origin.replace(/^http/, "ws") + "/websocket");
 
-        websocket.onopen = function () {
-            console.log("connection opened");
-            websocket.send(usernameValue());
-        }
-
-        websocket.onerror = function (e) {
-            setTextAreaValue(value => value + "Connection error occured\n\n");
-        }
-
-        websocket.onclose = function () {
-            console.log("connection closed");
-            setConnectDisabled(false);
-        }
-
-        websocket.onmessage = function (e) {
-            const chatBotPartialPrefix = "[CHAT_PARTIAL]: ";
-            const chatBotCompletedPrefix = "Chat: ";
-            const getMatchesOld = value => /(.*\n\n)(Chat: .*?　)(\n\n.*?)/.exec(value);
-            const getMatches = value => {
-                const padIdx = value.lastIndexOf("　");
-                if (padIdx < 0) return null;
-                const preChatStartIdx = value.substring(0, padIdx).lastIndexOf("\n\nChat: ");
-                if (preChatStartIdx < 0) return null;
-                const chatStartIdx = preChatStartIdx + 2;
-                const chatEndIdx = padIdx + 1;
-                return [value, value.substring(0, chatStartIdx), value.substring(chatStartIdx, chatEndIdx), value.substring(chatEndIdx)]
-            };
-
-            if (e.data.startsWith(chatBotPartialPrefix)) {
-                let partial = e.data.substring(chatBotPartialPrefix.length);
-                setTextAreaValue(value => {
-                    const matches = getMatches(value);
-                    if (matches) {
-                        return `${matches[1]}Chat: ${partial}　${matches[3]}`;
-                    } else {
-                        return value;
-                    }
-                });
-                return;
+            websocket.onopen = function () {
+                console.log("connection opened");
+                websocket.send(usernameValue());
+                resolve(true);
             }
 
-            if (e.data.startsWith(chatBotCompletedPrefix)) {
-                const value = textAreaValue();
-                const matches = getMatches(value);
-                if (matches) {
-                    console.log("received message: " + e.data);
-                    setTextAreaValue(value => `${matches[1]}${e.data}${matches[3]}`);
+            websocket.onerror = function (e) {
+                setTextAreaValue(value => value + "Connection error occured\n\n");
+                reject(e);
+            }
+
+            websocket.onclose = function () {
+                console.log("connection closed");
+                setConnectDisabled(false);
+            }
+
+            websocket.onmessage = function (e) {
+                const chatBotPartialPrefix = "[CHAT_PARTIAL]: ";
+                const chatBotCompletedPrefix = "Chat: ";
+                const getMatchesOld = value => /(.*\n\n)(Chat: .*?　)(\n\n.*?)/.exec(value);
+                const getMatches = value => {
+                    const padIdx = value.lastIndexOf("　");
+                    if (padIdx < 0) return null;
+                    const preChatStartIdx = value.substring(0, padIdx).lastIndexOf("\n\nChat: ");
+                    if (preChatStartIdx < 0) return null;
+                    const chatStartIdx = preChatStartIdx + 2;
+                    const chatEndIdx = padIdx + 1;
+                    return [value, value.substring(0, chatStartIdx), value.substring(chatStartIdx, chatEndIdx), value.substring(chatEndIdx)]
+                };
+
+                if (e.data.startsWith(chatBotPartialPrefix)) {
+                    let partial = e.data.substring(chatBotPartialPrefix.length);
+                    setTextAreaValue(value => {
+                        const matches = getMatches(value);
+                        if (matches) {
+                            return `${matches[1]}Chat: ${partial}　${matches[3]}`;
+                        } else {
+                            return value;
+                        }
+                    });
                     return;
+                }
+
+                if (e.data.startsWith(chatBotCompletedPrefix)) {
+                    const value = textAreaValue();
+                    const matches = getMatches(value);
+                    if (matches) {
+                        console.log("received message: " + e.data);
+                        setTextAreaValue(value => `${matches[1]}${e.data}${matches[3]}`);
+                        return;
+                    }
+                }
+
+                console.log("received message: " + e.data);
+                setTextAreaValue(value => value + e.data + "\n\n");
+                if (chatTextArea) {
+                    chatTextArea.scrollTop = chatTextArea.scrollHeight;
                 }
             }
 
-            console.log("received message: " + e.data);
-            setTextAreaValue(value => value + e.data + "\n\n");
-            if (chatTextArea) {
-                chatTextArea.scrollTop = chatTextArea.scrollHeight;
-            }
-        }
-
-        setWsInvokeSend(_ => ({ send: data => websocket.send(data) }));
+            setWsInvokeSend(_ => ({ send: data => websocket.send(data) }));
+        });
     };
+
+    const onConnectClick = () => {
+        wsStartConnection();
+    }
 
     const onUsernameKeyDown = e => {
         if (e.key == "Enter") {
             e.preventDefault();
-            onConnect();
+            onConnectClick();
         }
     };
 
@@ -117,7 +127,15 @@ function Conversation() {
         }
     };
 
-    const onMessageSubmit = () => {
+    const onMessageSubmit = async () => {
+        if (!connectDisabled()) {
+            try {
+                await wsStartConnection();
+            }
+            catch {
+                return;
+            }
+        }
         const { send } = wsInvokeSend();
         const msg = messageInputValue();
         if (send && msg) {
@@ -138,7 +156,7 @@ function Conversation() {
                         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
                     </svg>
                     </div>
-                    <form onsubmit=${onConnect}>
+                    <form onsubmit=${onConnectClick}>
                         <input class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                             required placeholder="Username" type="text" id="website-admin"
                             value=${usernameValue}
@@ -146,7 +164,7 @@ function Conversation() {
                             onKeyDown=${onUsernameKeyDown}>
                     </form>
                     <button class="text-white absolute right-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                        type="submit" disabled=${connectDisabled} onClick=${onConnect}>
+                        type="submit" disabled=${connectDisabled} onClick=${onConnectClick}>
                             Join Chat
                     </button>
                 </div>
