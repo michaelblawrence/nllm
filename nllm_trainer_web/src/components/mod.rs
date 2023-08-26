463e25124e3417a06eabf3566cee6249cb11fe36
Michael Lawrence <mblawrence27@gmail.com>
2023-08-26 14:56:21 +0900

    delete unused crates/bins

--- nllm_trainer_web/src/components/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod trainer;
-pub mod plot;
-pub mod editor;
-pub mod chatter;
-
-pub use trainer::{EmbeddingTrainer, RefCellHandle};
-pub use plot::PlotComponent;
-pub use editor::JsonEditor;
-pub use chatter::EmbeddingChat;
